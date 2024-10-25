247c0b249aeb3c42bef04b8725fd03cc8b6a88c3
ook37 <oren+12345@taumoda.com>
2024-10-24 20:59:15 -0400

    fix goof

--- generate-sizes.sh
+++ generate-sizes.sh
@@ -2,12 +2,16 @@
 
 sizes=(16 18 22 24 32 42 48 64 84 96 128)
 
+for s in "${sizes[@]}"; do
+  size="${s}x${s}/apps"
+  mkdir -p "${size}"
+  rm -rf "${size}"/*
+done
+
 for icon in scalable/apps/*.svg; do
   iconname=$(basename "${icon}" .svg)
   for s in "${sizes[@]}"; do
     size="${s}x${s}/apps"
-    mkdir -p "${size}"
-    rm -rf "${size}"/*
     iconsize="${size}/${iconname}.png"
     echo "Generating ${iconsize}..."
     if command -v rsvg-convert > /dev/null; then
